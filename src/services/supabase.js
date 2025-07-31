f9513529f0702818a182e938bff0b63dabf82aa0
Alina <alina_gabur@yahoo.com>
2025-07-31 13:47:19 +0200

    .env toegevoegd

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
- export const supabaseUrl = 'https://lnzgeslnvfwflnrntdhy.supabase.co';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxuemdlc2xudmZ3Zmxucm50ZGh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2Njc0MDcsImV4cCI6MjA2ODI0MzQwN30.CIhUnjk-g7nQ0eelDQthrj1Fzk_8RQ_tTXQi62ueMrA";
+ export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+ const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
